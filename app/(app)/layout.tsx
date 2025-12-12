114f4efec849dea8edc424efeb0a8f271a3221b0
Cypherfelix <felixmarvinonline@gmail.com>
2025-12-12 15:25:44 +0300

    feat(layout): ✨ enhance AppLayout with Header and Toaster components for improved user experience

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,11 +1,15 @@
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
+import { Header } from "@/components/app/Header";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
+        <Header />
         <main>{children}</main>
+        <Toaster position="top-right" richColors />
       </CartStoreProvider>
     </ClerkProvider>
   );
